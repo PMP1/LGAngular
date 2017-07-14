1e82dfceed7c39ff1ca170ff280ab4ab19f228cb
Phil Pratt <phil.pratt@hub4leaders.co.uk>
2017-07-14 11:21:50 +0100

    temp commit

---
+++ LGAngular/Client/dist/app.js
@@ -0,0 +1,11 @@
+webpackJsonp([1],{
+
+/***/ 213:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'C:\\Users\\phil.pratt\\documents\\visual studio 2017\\Projects\\LGAngular\\LGAngular\\client\\main.ts'\n    at Error (native)");
+
+/***/ })
+
+},[213]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VSb290IjoiIn0=
